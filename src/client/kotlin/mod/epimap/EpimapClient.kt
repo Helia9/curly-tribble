22a5331f0a4a3580732d63c9dfba9abaa541e3ae
Helia <bach-long.huynh@epitech.eu>
2024-12-19 15:55:33 +0100

    first commit

---
+++ src/client/kotlin/mod/epimap/EpimapClient.kt
@@ -0,0 +1,9 @@
+package mod.epimap
+
+import net.fabricmc.api.ClientModInitializer
+
+object EpimapClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
